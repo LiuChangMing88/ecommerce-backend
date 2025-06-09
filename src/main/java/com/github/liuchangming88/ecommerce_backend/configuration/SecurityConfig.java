cd5299cf279932751b00c28e1720c5fd44d22daa
LiuChangMing88 <lautruongminh@gmail.com>
2025-06-09 17:26:25 +0700

    feat(auth): expose OAuth2 endpoints and configure OAuth2 login in security chain

--- src/main/java/com/github/liuchangming88/ecommerce_backend/configuration/SecurityConfig.java
+++ src/main/java/com/github/liuchangming88/ecommerce_backend/configuration/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.github.liuchangming88.ecommerce_backend.configuration;
 
 import com.github.liuchangming88.ecommerce_backend.security.JwtRequestFilter;
-import com.github.liuchangming88.ecommerce_backend.service.CustomOAuth2UserService;
+import com.github.liuchangming88.ecommerce_backend.security.oauth2.UserHandler.CustomOAuth2UserService;
+import com.github.liuchangming88.ecommerce_backend.security.oauth2.OAuth2AuthenticationSuccessHandler;
+import com.github.liuchangming88.ecommerce_backend.security.oauth2.UserHandler.CustomOidcUserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
@@ -19,16 +21,19 @@ import org.springframework.security.web.authentication.AuthenticationFilter;
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
-public class WebSecurityConfig {
+public class SecurityConfig {
     private final JwtRequestFilter jwtRequestFilter;
     private final CustomOAuth2UserService customOAuth2UserService;
+    private final CustomOidcUserService customOidcUserService;
     private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
 
-    public WebSecurityConfig(JwtRequestFilter jwtRequestFilter,
+    public SecurityConfig(JwtRequestFilter jwtRequestFilter,
                           CustomOAuth2UserService customOAuth2UserService,
+                          CustomOidcUserService customOidcUserService,
                           OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler) {
         this.jwtRequestFilter = jwtRequestFilter;
         this.customOAuth2UserService = customOAuth2UserService;
+        this.customOidcUserService = customOidcUserService;
         this.oAuth2AuthenticationSuccessHandler = oAuth2AuthenticationSuccessHandler;
     }
 
@@ -44,7 +49,8 @@ public class WebSecurityConfig {
                 )
                 .oauth2Login(oauth2 -> oauth2
                         .userInfoEndpoint(userInfo -> userInfo
-                                .userService(customOAuth2UserService)
+                                .userService(customOAuth2UserService)           // for “code” (GitHub, etc.)
+                                .oidcUserService(customOidcUserService)         // for OIDC (Google)
                         )
                         .successHandler(oAuth2AuthenticationSuccessHandler)
                 )
